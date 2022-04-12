c9419c202e6fe8f0916217aaafc410a78cb79830
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-04-12 11:18:34 -0500

    Microservicio mdyagual - Initial commit

---
+++ MicrofacturarSK/src/main/java/com/java/microfacturar/MicroFacturarSkApplication.java
@@ -0,0 +1,13 @@
+package com.java.microfacturar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroFacturarSkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroFacturarSkApplication.class, args);
+	}
+
+}
---
+++ MicrofacturarSK/src/test/java/com/java/microfacturar/MicroFacturarSkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.microfacturar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroFacturarSkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
