3570b647cbffc4acf92f1a2dc569ae8ce5c1103a
lucia2207 <ludef15@gmail.com>
2022-04-12 16:20:14 -0300

    Agregado RabbitConfig y configuracion en el application.properties

---
+++ MicroLu/src/main/java/com/example/demo/config/RabbitConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.config;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+public class RabbitConfig {@Value("${spring.rabbitmq.host}")
+private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private Integer port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String user;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.virtualhost}")
+    private String virtualhost;
+
+    @Bean
+    public ConnectionFactory connectionFactory() throws Exception {
+        final CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setAddresses(host);
+        connectionFactory.setPort(port);
+        connectionFactory.setUsername(user);
+        connectionFactory.setPassword(password);
+        //connectionFactory.setVirtualHost(virtualhost);
+        return connectionFactory;
+    }
+}
