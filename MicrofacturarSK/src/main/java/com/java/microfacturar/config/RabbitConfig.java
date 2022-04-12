5573dbe3d2c12c53b97faa2aab97e10cece9275b
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-04-12 14:10:53 -0500

    Configuración del uso de rabbitMQ - Funcionalidad ok con credenciales diferentes a guest

---
+++ MicrofacturarSK/src/main/java/com/java/microfacturar/config/RabbitConfig.java
@@ -0,0 +1,36 @@
+package com.java.microfacturar.config;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private Integer port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String user;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.virtualhost}")
+    private String virtualhost;
+
+    @Bean
+    public ConnectionFactory connectionFactory() throws Exception {
+        final CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setAddresses(host);
+        connectionFactory.setPort(port);
+        connectionFactory.setUsername(user);
+        connectionFactory.setPassword(password);
+        //connectionFactory.setVirtualHost(virtualhost);
+        return connectionFactory;
+    }
+}
